bcfc22a1333212871deb04b553d7c5d49128d73a
Thinus <thinus.barnard@payfast.co.za>
2022-08-28 00:01:58 +0200

    Clean up files

--- resources/js/Pages/Login.vue
+++ resources/js/Pages/Login.vue
@@ -54,62 +54,13 @@ const submitForm = async (event) => {
 </script>
 
 <style scoped lang="scss">
-.login-page {
-    width: 100%;
-    padding: 20px;
-}
-/* Add padding to container elements */
 .container {
     padding: 20px;
     max-width:500px;
     margin: 100px auto auto;
-    border:1px solid #ccc;
-    border-radius:10px;
-    background:white;
-    -webkit-box-shadow: 2px 1px 21px -9px rgba(0,0,0,0.38);
-    -moz-box-shadow: 2px 1px 21px -9px rgba(0,0,0,0.38);
-    box-shadow: 2px 1px 21px -9px rgba(0,0,0,0.38);
 }
 
-/* Full-width input fields */
-input[type=text], input[type=password] {
-    width: 100%;
-    padding: 15px;
-    margin: 5px 0 22px 0;
-    display: inline-block;
-    border: none;
-    background: #f7f7f7;
-    font-family: Montserrat,Arial, Helvetica, sans-serif;
-}
-
-input[type=text]:focus, input[type=password]:focus, input[type=phone]:focus, select:focus {
-    background-color: #ddd;
-    outline: none;
-}
-
-button {
-    background-color: #0eb7f4;
-    color: white;
-    padding: 14px 20px;
-    margin: 8px 0;
-    border: none;
-    cursor: pointer;
-    width: 100%;
-    opacity: 0.9;
-    font-size:16px;
-    font-family: Montserrat,Arial, Helvetica, sans-serif;
-    border-radius:10px;
-}
-
-button:hover {
-    opacity:1;
-}
-
-
-/* Change styles for signup button on extra small screens */
-@media screen and (max-width: 300px) {
-    .signupbtn {
-        width: 100%;
-    }
+input {
+    display: block;
 }
 </style>
